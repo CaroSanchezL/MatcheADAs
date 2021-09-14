68eaaaefae5cd84191efce0f8f14fa21f7b3facf
Celina Alsina <celina.alsina@gmail.com>
2021-09-14 18:37:13 +0200

    tamano de las tres grillas con prompt de usuario

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 
+
 const emojisAvaibles = ["🐊","🐆","🐅","🐃","🐂","🐄", "🦬", "🐪","🦙","🐘","🦏","🦛","🦣","🐐","🐑", "🐎", "🐖", "🦇", "🐓","🦃","🕊","🦅","🦆"]
 
+//ELEMENTOS DEL DOM
+
+
+const matrixContainer = document.querySelector(".matrix-container")
+
+
+
+//FUNCIONES 
 
 let matrixMaker = (rows, columns, array) => {
   let matrix = [];
   for (let i = 0; i < rows; i++) {
     let subArray = [];
-
-
     for (let j = 0; j < columns; j++) {
       subArray.push(array[Math.trunc(Math.random()*array.length)])
-
     }
     matrix.push(subArray)
-
-
   }
   return matrix
 }
-
-console.log(matrixMaker(3,3, emojisAvaibles))
-
 
 
 
@@ -35,27 +36,46 @@ let stringInHtml = (array) => {
 
 }
 
+let easyLevel = matrixMaker(9,9,emojisAvaibles);
+let mediumLevel = matrixMaker(8,8,emojisAvaibles);
+let hardLevel = matrixMaker(7,7, emojisAvaibles);
 
 
 
 
-const matrixContainer = document.querySelector(".matrix-container")
-matrixContainer.innerHTML = stringInHtml(matrixMaker(5,5, emojisAvaibles))
+const level = prompt("Fácil, medio o difícil")
 
 
 
-// /* const generarStringHTML = (array) => {
-//   let resultado = ""
 
-//   for (let i = 0; i < array.length; i++) {
-//     resultado = resultado + `<div class="item">${array[i]}</div>`
-    
-//   }
-//   return resultado
-// }
+if (level == "facil") {
+  matrixContainer.innerHTML = stringInHtml(easyLevel)
+  const emojisInMatrix = document.querySelectorAll(".item");
+  for (let i = 0; i < emojisInMatrix.length ; i++) {
+    emojisInMatrix[i].style.fontSize = "12px";
+  }
+}
+
+if (level == "medio") {
+  matrixContainer.innerHTML = stringInHtml(mediumLevel);
+  const emojisInMatrix = document.querySelectorAll(".item");
+  for (let i = 0; i < emojisInMatrix.length ; i++) {
+    emojisInMatrix[i].style.fontSize = "15px";
+    emojisInMatrix[i].style.width ="22px"
+  }
+}
+
+if (level == "dificil") {
+  matrixContainer.innerHTML = stringInHtml(hardLevel);
+  const emojisInMatrix = document.querySelectorAll(".item");
+  for (let i = 0; i < emojisInMatrix.length ; i++) {
+    emojisInMatrix[i].style.fontSize = "17px";
+    emojisInMatrix[i].style.width = "25px";
+  }
+}
 
 
-// const grilla = document.querySelector(".grilla")
 
-// grilla.innerHTML = generarStringHTML(["🍋", "🍋","🍋", "🍋","🍋","🍋",])
-//  
+
+
+
